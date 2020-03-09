0281e5128d1f274775c3ea66e9d4b2323586faa7
mav-code <mavisansmembres@gmail.com>
2020-03-09 12:15:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (num) {
+    return Math.abs(42 - num)
+  }
+   
+  function distanceFromHqInFeet (num) {
+    return distanceFromHqInBlocks(num) * 264
+  }
+
+  function distanceTravelledInFeet (block1, block2) {
+    return Math.abs(block1 - block2) * 264
+  }
+
+  function calculatesFarePrice (block1, block2) {
+      let distance = distanceTravelledInFeet(block1, block2)
+      switch (true) {
+        case (distance <= 400):
+            return 0
+            break
+        case (distance <= 2000):
+            return (distance - 400) * 0.02
+            break
+        case (distance <= 2500):
+            return 25
+            break
+        case (distance > 2500):
+            return 'cannot travel that far'
+            break
+
+      }
+  }
